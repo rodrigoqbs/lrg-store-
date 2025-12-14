82b352848d47eb095322a140a6ee0d7a7a73372d
rodrigoqbs <rodrigobigaisanchez@outlook.com>
2025-12-14 20:45:02 -0300

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20, fontFamily: "Arial" }}>
+      <h1>📦 LRG STORE</h1>
+      <p>Aplicação carregada com sucesso.</p>
+    </div>
+  );
+}
